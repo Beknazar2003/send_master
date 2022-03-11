0cf399a31a5088199ba7e7b34c098723a27b6834
dev_ametov_alisher <a.ametov@2-up.ru>
2022-03-11 15:37:56 +0600

    geoip

--- routes/router.js
+++ routes/router.js
@@ -1,28 +1,38 @@
 const express = require('express')
+const {lookup} = require('geoip-lite')
 const router = express.Router()
-const {apiSendMessageToChat, botSendMessageToChat} = require("../bot");
+const {apiSendMessageToChat, botSendMessageToChat} = require('../bot');
 
 router.get('*', (req, res) => {
-  res.render('index')
+    res.render('index')
 })
 router.post('/send', async (req, res) => {
-  const {body} = req
-  let message = ''
-  for (let key in body) {
-    message += `${key}: ${body[key]}\n`
-  }
-  await apiSendMessageToChat(message)
-  res.status(200)
-  res.json({result: {message: body}})
+    const {body} = req
+    let message = ''
+    for (let key in body) {
+        message += `${key}: ${body[key]}\n`
+    }
+    await apiSendMessageToChat(message)
+    res.status(200)
+    res.json({result: {message: body}})
 })
 router.post('/bot/send', async (req, res) => {
-  const {body} = req
-  let message = ''
-  for (let key in body) {
-    message += `${key}: ${body[key]}\n`
-  }
-  await botSendMessageToChat(message)
-  res.status(200)
-  res.json({result: {message: body}})
+    const {body} = req
+    let message = ''
+    for (let key in body) {
+        message += `${key}: ${body[key]}\n`
+    }
+    await botSendMessageToChat(message)
+    res.status(200)
+    res.json({result: {message: body}})
+})
+router.get('/bot/get-ip-info', async (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    res.json({
+        result: {
+            ip,
+            info: lookup(ip)
+        }
+    })
 })
 module.exports = router
