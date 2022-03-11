4809a1ff4736559a41f55995cbc48d81db486e6f
dev_ametov_alisher <a.ametov@2-up.ru>
2022-03-11 15:44:30 +0600

    geoip

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,6 @@ const {lookup} = require('geoip-lite')
 const router = express.Router()
 const {apiSendMessageToChat, botSendMessageToChat} = require('../bot');
 
-router.get('*', (req, res) => {
-    res.render('index')
-})
 router.post('/send', async (req, res) => {
     const {body} = req
     let message = ''
@@ -27,12 +24,17 @@ router.post('/bot/send', async (req, res) => {
     res.json({result: {message: body}})
 })
 router.get('/bot/get-ip-info', async (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const ip = req.socket.remoteAddress;
+    console.log(ip)
     res.json({
         result: {
             ip,
             info: lookup(ip)
         }
     })
+})
+
+router.get('*', (req, res) => {
+    res.render('index')
 })
 module.exports = router
